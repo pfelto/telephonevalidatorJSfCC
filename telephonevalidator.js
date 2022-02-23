d9eb6fea7d9fee8980407a51b8c9090dc1a8fab4
pfelto <paul.r.felton@gmail.com>
2022-02-23 09:58:27 -0500

    Completed all the test cases in freeCodeCamp. Believe this code is a little redundent and might not check everything, but code for what you need I guess

--- telephonevalidator.js
+++ telephonevalidator.js
@@ -1,25 +1,31 @@
 function telephoneCheck(str) {
+  let digitCount = str.match(/\d/g).length;
+
   if (str.length < 10) return false;
 
   if (/[^ -\d)(]/g.test(str)) return false;
 
-  //Could also have a counter for number of numbers in str? and if those are not both 10 false?
-  //need the number of digits later as if there are 11 then 1st place has to be a 1
+  if (digitCount > 11) return false;
+
+  if (digitCount === 11 && !/^1/.test(str)) return false;
+
   if (str.length === 10 && !/^\d+$/.test(str)) return false;
 
   if (str.length === 11 && !/^1\d+$/.test(str)) return false;
 
   if (str.length >= 12) {
     if (!/-\d\d\d\d$| \d\d\d\d$/.test(str)) return false;
+    if (/\(/.test(str) || /\)/.test(str)) {
+      if (!/\(\d{3}\)/.test(str)) return false;
+    }
   }
 
   return true;
 }
 
-//console.log(telephoneCheck("555-555-5555"));
-//console.log(telephoneCheck("555 555 5555"));
-//console.log(telephoneCheck("555 5555 5 55"));
-console.log(telephoneCheck("123**&!!asdf#"));
-console.log(telephoneCheck("5555555555"));
+console.log(telephoneCheck("555)-555-5555"));
+console.log(telephoneCheck("(555-555-5555"));
+console.log(telephoneCheck("(555)555-5555"));
+console.log(telephoneCheck("555-555-5555"));
 
 module.exports = telephoneCheck;
